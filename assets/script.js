a771fc68204f0cbc7b425fb65ded61c5d6e92379
Matilda Bakare <matildabakare@gmail.com>
2021-05-10 00:25:13 -0400

    added password length portion

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+//Create a function "generatePassword()" that will execute the actions required 
+var generatePassword = function () {
 
+  //Prompt for password length
+  var passwordLength = window.prompt(
+    "What is the length of your password? (Enter a number between 8 and 128)"
+  );
+  //Make the prompt input an integer rather than a string
+  passwordLength = parseInt(passwordLength);
+
+  //Ensuring the password length entered is within the requirements
+  if (!(passwordLength >= 8 && passwordLength <= 128)) {
+    window.alert("You did not pick a valid option. Try again.");
+    return generatePassword ();
+  }
+
+  else {
+    //function
+  }
+
+
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +37,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
